3877eb45783fab3bf662f9b8e72d93f47bcd4874
Mike DeGan <mikedegan@gmail.com>
2018-12-10 19:18:17 -0500

    Updates

---
+++ precache-manifest.45eb4e241813706fc9dab26d26e7a769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testghpages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "15f3e21f0b19caef5c64",
+    "url": "/testghpages/static/js/runtime~main.15f3e21f.js"
+  },
+  {
+    "revision": "004f47bfb60b074d3ec0",
+    "url": "/testghpages/static/js/main.004f47bf.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/testghpages/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "004f47bfb60b074d3ec0",
+    "url": "/testghpages/static/css/main.100270ca.chunk.css"
+  },
+  {
+    "revision": "c73beafcc83cfe98a6e5af1bb4369f7a",
+    "url": "/testghpages/index.html"
+  }
+];
